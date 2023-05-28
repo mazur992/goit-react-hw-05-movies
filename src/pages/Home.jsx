caf790c4dfcd4026737ce6e7b00eb8a13ef89512
Mazur Roman <superioritywork@gmail.com>
2023-05-28 11:05:52 +0200

    add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,50 @@
-export default function Home() {return(<div>Home</div>)}
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import axios from 'axios';
+export default function Home() {
+  const [movies, setMovies] = useState(null);
+
+  async function getMoviesTop() {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/trending/all/day',
+      params: { language: 'en-US' },
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjM2YWJkZmM3NDE4MTQ0MTZlZDNhOWQ3OGZkMzNiNiIsInN1YiI6IjY0MzU1ZDlmZWM4YTQzMDIxOTI2NzJhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JLPbYzf2ZSUL6iP2RY-vf-bExRMCVBdbt1ajwYeenmE',
+      },
+    };
+    return await axios
+      .request(options)
+      .then(async function (response) {
+        return response.data;
+      })
+      .then(async function (data) {
+        setMovies(await data.results);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+  useEffect(() => {
+    getMoviesTop();
+  }, []);
+
+  return (
+    <>
+      <h1>Trending today</h1>
+      <ul>
+        {movies &&
+          movies.map(movie => {
+            return (
+              <li key={movie.id}>
+                <Link to={`movies/${movie.id}`}>{movie.title}</Link>
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
+}
--- src/services/movies_service.js
+++ src/services/movies_service.js
@@ -1,50 +1,36 @@
-export default function MoviesApiService() {
-  const TMDB_API = 'https://api.themoviedb.org/3/';
-  const MY_TMDB_KEY = '3f36abdfc741814416ed3a9d78fd33b6';
+import axios from 'axios';
 
-  // Ключове слово. Запит
-  async function getMoviesSearch() {
-    return fetch(
-      `${TMDB_API}search/keyword?api_key=${MY_TMDB_KEY}&query=${searchQuery}`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+// Тренди дня. Запит
+export async function getMoviesTop() {
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/trending/all/day',
+    params: { language: 'en-US' },
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjM2YWJkZmM3NDE4MTQ0MTZlZDNhOWQ3OGZkMzNiNiIsInN1YiI6IjY0MzU1ZDlmZWM4YTQzMDIxOTI2NzJhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JLPbYzf2ZSUL6iP2RY-vf-bExRMCVBdbt1ajwYeenmE',
+    },
+  };
+  return await axios
+    .request(options)
+    .then(async function (response) {
+      return response.data;
+    })
+    .then(async function (data) {
+      return await data.results;
+    })
+    .catch(function (error) {
+      console.error(error);
     });
-  }
-
-  // Тренди дня. Запит
-  async function getMoviesTop() {
-    return fetch(
-      `${TMDB_API}trending/movie/day?api_key=${MY_TMDB_KEY}&id`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  }
-  // Окремий фільм. Запит
-  async function getMovieId(id) {
-    return fetch(
-      `${TMDB_API}movie/${id}?api_key=${MY_TMDB_KEY}&language=en-US`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  }
-  // Трейлер. Запит
-  async function getMovieTrailerbyId(id) {
-    return fetch(
-      `${TMDB_API}movie/${id}/videos?api_key=${MY_TMDB_KEY}&language=en-US`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  }
 }
+// async function getMoviesTop() {
+
+// }
+//   // Ключове слово. Запит
+//   async function getMoviesSearch() {}
+
+//   // Окремий фільм. Запит
+//   async function getMovieId(id) {}
+//   // Трейлер. Запит
+//   async function getMovieTrailerbyId(id) {}
