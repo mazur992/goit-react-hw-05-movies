a8062aaaa38a9e19fcd548536182a37364479012
Mazur Roman <superioritywork@gmail.com>
2023-05-30 20:59:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mazur992">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
